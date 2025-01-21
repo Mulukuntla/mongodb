a17943b0c761a15b2bf0a8a43e9fa83e46b481b3
Mulukuntla <yours.saisaketh@gmail.com>
2025-01-21 16:26:38 +0530

    creating user table

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User=require("./models/user")
 
 const app = express();
 
@@ -18,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("678f775d0d991d98f7890141")
+    .then(user => {
+       req.user = user;
+       next();
+     })
+    .catch(err => console.log(err));
+  
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb=require("mongodb")
+const getdb=require("../util/database").getDb;
+const ObjectId=mongodb.ObjectId
+class User{
+  constructor(username,email){
+    this.name=username
+    this.email=email
+  }
+  save(){
+    const db=getdb()
+    return db.collection("users").insertOne(this)
+  }
+  static findById(userId){
+    const db=getdb()
+    return db.collection("users")
+      .findOne({_id:new ObjectId(userId)})
+      .then(user =>{
+        console.log(user)
+        return user
+      })
+      .catch(err =>{
+        console.log(user)
+      })
 
-const sequelize = require('../util/database');
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
 
-module.exports = User;
+}
+module.exports=User;
