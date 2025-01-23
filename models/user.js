ac6b2e7f3188ff1070bd439014a3c8521ea901a1
Mulukuntla <yours.saisaketh@gmail.com>
2025-01-23 14:58:39 +0530

    relation between tables

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb=require("mongodb")
 const getdb=require("../util/database").getDb;
 const ObjectId=mongodb.ObjectId
 class User{
-  constructor(username,email){
+  constructor(username,email,cart,id){
     this.name=username
     this.email=email
+    this.cart=cart
+    this._id=id
   }
   save(){
     const db=getdb()
@@ -19,8 +21,36 @@ class User{
         return user
       })
       .catch(err =>{
-        console.log(user)
+        console.log(err)
       })
+
+  }
+  addToCart(product){
+    const cartProductIndex=this.cart.items.findIndex(cp =>{
+      return cp.productId.toString()===product._id.toString()
+    });
+    let newQuantity=1
+    const updatedCartItems=[...this.cart.items]
+    if(cartProductIndex >= 0){
+      newQuantity=this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity=newQuantity
+
+    }
+    else{
+      updatedCartItems.push({productId:new ObjectId(product._id),quantity:newQuantity})
+
+    }
+    const updatedCart={
+      items:updatedCartItems
+    }
+    const db=getdb()
+    return db
+      .collection("users")
+      .updateOne(
+        {_id:new ObjectId(this._id)},
+        {$set:{cart:updatedCart}}
+      )
+
 
   }
 
