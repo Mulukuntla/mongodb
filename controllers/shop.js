6f078a915af0a42b84ce989e76bcf3b52a337f1a
Mulukuntla <yours.saisaketh@gmail.com>
2025-01-25 17:41:21 +0530

    cart system

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,9 +54,11 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
-      console.log("array---------------->",products)
+    .populate("cart.items.productId")
+    //.execPopulate()
+    .then(user => {
+      console.log("array----------->",user.cart.items)
+      const products=user.cart.items
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
